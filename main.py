9ac4d7ba8a22a2996f928bb0b724866277023df5
John <exampl@john.com>
2022-12-24 21:09:28 +0000

    regular commit

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ app=FastAPI()
 def read_root():
     return {'hello':'world'}
 
-     
+@app.get('/course/{course_id}') #now course_id can be used in function parameter
+def my_course(course_id:int):
+    return {'course_id':course_id}
+
+dummy_data=[i for i in range(100)]
+
+@app.get('/my/page/items/')
+async def read_item(page: int=0, limit:int=0,skip:int=1):
+    return dummy_data[page*10: page*10 + limit:skip]
