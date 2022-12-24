9f65d2f310c0159e7fe59520b3a39e76fc87eb6f
John <exampl@john.com>
2022-12-24 21:21:40 +0000

    regular commit

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ dummy_data=[i for i in range(100)]
 @app.get('/my/page/items/')
 async def read_item(page: int=0, limit:int=0,skip:int=1):
     return dummy_data[page*10: page*10 + limit:skip]
+
+from pydantic import BaseModel
+class MyItem(BaseModel):
+    name:str
+    info:str=None
+    price:float
+    qty:int
+
+@app.post('/purchase/item/')
+async def create_item(item:MyItem):
+    return {'amount':item.qty*100,'success':True}
